5dd900e2820d77c4bf388042acb734fc39ff7cca
Yoel Ivan <yivan3@gatech.edu>
2016-09-25 02:23:36 +0100

    merge gm wrapper api to the server

---
+++ server/src/controllers/gm.js
@@ -0,0 +1,83 @@
+import request from "request";
+import { API_GM_KEY as idpass } from "../config";
+
+export const VIN = {
+    chevyEquinox : "2CNALPEC3B6000001",
+    chevyCamaro  : "1G6DH5E53C000000",
+    gmcSierra    : "1G1PJ5SC9C700000",
+    chevySuburban: "1GCRCSE09BZ00000",
+    chevyMalibu  : "1G1ZE5E03CF000006",
+    chevyYukon   : "1G1JE6SH2C4000007"
+};
+
+export default function gm(vin, cb) {
+    // Get the bearer key
+    var bearer_key = {
+        method : "GET",
+        url    : "https://developer.gm.com/api/v1/oauth/access_token",
+        qs     : {
+            grant_type: "client_credentials"
+        },
+        headers: {
+            "cache-control": "no-cache",
+            accept         : "application/json",
+            authorization  : "Basic " + idpass
+        }
+    };
+
+    request(bearer_key, function (error, response, body) {
+        if (error) {
+            throw new Error(error);
+        }
+        var bearerkey = JSON.parse(body).access_token;
+        var options = {
+            method : "POST",
+            url    : "https://developer.gm.com/api/v1/account/vehicles/"
+            + vin + "/commands/diagnostics",
+            headers: {
+                "cache-control": "no-cache",
+                "content-type" : "application/json",
+                accept         : "application/json",
+                authorization  : "Bearer " + bearerkey
+            },
+            body   : {
+                diagnosticsRequest: {
+                    diagnosticItem: [
+                        "FUEL TANK INFO",
+                        "VEHICLE RANGE"
+                    ]
+                }
+            },
+            json   : true
+        };
+        request(options, function (error, response, body) {
+            if (error) {
+                throw new Error(error);
+            }
+            console.log(body.commandResponse.url);
+            var diagnostic = {
+                method : "GET",
+                url    : body.commandResponse.url,
+                headers: {
+                    "cache-control": "no-cache",
+                    accept         : "application/json",
+                    authorization  : "Bearer " + bearerkey
+                }
+            };
+            var intID = setInterval(function () {
+                request(diagnostic, function (error, response, body) {
+                    if (error) {
+                        throw new Error(error);
+                    }
+                    body = JSON.parse(body);
+                    if (body.commandResponse.status !== "inProgress") {
+                        clearInterval(intID);
+                        cb(body);
+                    }
+                });
+            }, 4000);
+
+        });
+    });
+}
+
--- server/src/routes/gas.js
+++ server/src/routes/gas.js
@@ -2,13 +2,14 @@
 "use strict";
 
 import { Router } from "express";
+import gm, {VIN} from "../controllers/gm";
 
 const router = Router();
 
 router.get("/", (req, resp) => {
     let origin = "41.99668,-87.87675";
     let destination = "41.81424,-86.69919";
-
+    gm(VIN.chevyMalibu, x => console.log(JSON.stringify(x)));
 });
 
 export default router;
